350500f340a609f35a5796ba0e59dba77628abcd
dhvendev <122101069+dhvendev@users.noreply.github.com>
2023-02-24 23:28:12 +0300

    new debugging task

---
+++ debug2.py
@@ -0,0 +1,5 @@
+
+#Задача: Исправить ошибки и вывести порядковый номер элемента, элемент и его длину.
+random_words = ['Apple', 'Juice', 'Tomato']
+for pos, element in enumerate(range(list)):
+    print(f"{pos + 1},{element} {len(element}")
